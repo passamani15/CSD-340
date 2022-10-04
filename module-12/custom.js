346e1ce01acb0b7ea3c2fe4e2ebf0e9d366fb50e
Larissa Passamani <102190991+passamani15@users.noreply.github.com>
2022-10-04 08:30:43 -0400

    changes

---
+++ module-12/custom.js
@@ -0,0 +1,23 @@
+
+
+
+// When the menu item is clicked, get the #id from the href value, then scroll to the #id element
+
+
+$(document).ready(function(){ 
+   
+    // $('#scroll').click(function(){ 
+    //     $("html, body").animate({ scrollTop: 0 }, 600); 
+    //     return false; 
+    // }); 
+
+    
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 400);
+    return false;
+});
+
+
+});
